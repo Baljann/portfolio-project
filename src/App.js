4e444a4056b21da591374b6568322a025dfd5581
Baljan Kanatbek <baljann19@gmail.com>
2025-05-06 22:42:10 +0300

    Implement routing in App component and update Navbar links to use NavLink

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./styles/main.css";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
@@ -9,13 +11,20 @@ import Project from "./pages/Project";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />;{/*<Home />*/}
-      {/* <Projects /> */}
-      <Project />
-      {/*<Contacts />*/}
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/projects" element={<Projects />}/>
+          <Route path="/project" element={<Project />}/>
+          <Route path="/contacs" element={<Contacts />}/>
+        </Routes>
+       
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
